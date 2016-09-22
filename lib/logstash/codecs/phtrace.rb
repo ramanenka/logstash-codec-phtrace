51f0226f8073f18092f9bdc6a9614ca5e1f62597
Vadzim Ramanenka <vramanenka@sugarcrm.com>
2016-09-22 11:20:17 +0200

    Parse request begin and end events

--- lib/logstash/codecs/phtrace.rb
+++ lib/logstash/codecs/phtrace.rb
@@ -1,6 +1,9 @@
 # encoding: utf-8
 require "logstash/codecs/base"
 
+class HeaderError < LogStash::Error; end
+class UnknownMessageError < LogStash::Error; end
+
 # Read events from the phtrace binary protocol over the network via udp.
 #
 # Configuration in your Logstash configuration file can be as simple as:
@@ -17,6 +20,8 @@ class LogStash::Codecs::Phtrace < LogStash::Codecs::Base
   config_name "phtrace"
 
   config :append, :validate => :string, :default => ', Hello World!'
+  
+  @current_request_uuid = false
 
   public
   def register
@@ -24,8 +29,53 @@ class LogStash::Codecs::Phtrace < LogStash::Codecs::Base
   
   public
   def decode(data)
-      replace = { "message" => data.to_s + @append }
-      yield LogStash::Event.new(replace)
+    pos = 0
+
+    while pos < data.bytesize do
+      type = data.byteslice(pos).unpack("C")[0]
+      pos += 1
+      
+      payloadLength = data.byteslice(pos..pos + 4).unpack("V")[0];
+      pos += 4
+
+      raise(HeaderError) if pos + payloadLength > data.bytesize 
+      
+      case type
+      when 1
+        event = decode_msg_request_begin data, pos, payloadLength 
+      when 2
+        event = decode_msg_request_end data, pos, payloadLength
+      else
+        raise(UnknownMessageError)
+      end
+      
+      pos += payloadLength
+      yield event
+    end
+  end
+  
+  protected
+  def decode_msg_request_begin(data, pos, payloadLength)
+    @current_request_uuid = uuid_unpack data, pos
+    pos += 16
+    
+    return LogStash::Event.new({
+      "_id" => @current_request_uuid,
+      "_type" => "request_begin"
+    })
+  end
+  
+  protected
+  def decode_msg_request_end(data, pos, payloadLength)
+    return LogStash::Event.new({
+      "_id" => @current_request_uuid,
+      "_type" => "request_end"
+    })
+  end
+  
+  protected
+  def uuid_unpack(data, pos)
+    return data.byteslice(pos..pos + 16).unpack("H8H4H4H4H12").join("-")
   end
 
 end
--- spec/codecs/phtrace_spec.rb
+++ spec/codecs/phtrace_spec.rb
@@ -8,8 +8,27 @@ describe LogStash::Codecs::Phtrace do
       next LogStash::Codecs::Phtrace.new({})
     end
 
-    it "should parse a normal packet" do
-      expect(28).to eq(28)
+    it "should parse begin and end request events" do
+      data = [
+        "01" + "10000000" + "298fa6e55d4b43ddb1fa89b0c4e97ac2" +
+        "02" + "00000000"
+      ].pack('H*')
+      
+      counter = 0;
+      subject.decode(data) do |event|
+        case counter
+        when 0
+          expect(event.get("_id")).to eq("298fa6e5-5d4b-43dd-b1fa-89b0c4e97ac2")
+          expect(event.get("_type")).to eq("request_begin")
+        when 1
+          expect(event.get("_id")).to eq("298fa6e5-5d4b-43dd-b1fa-89b0c4e97ac2")
+          expect(event.get("_type")).to eq("request_end")
+        end
+        
+        counter += 1
+      end
+      
+      expect(counter).to eq(2)
     end
   end 
 end
